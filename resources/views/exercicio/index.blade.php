05527482372acc6400e6ff7faa0bdb719686fc4c
Naoki Miura <naokirafael.miura@gmail.com>
2024-09-01 14:59:25 -0300

    style(view): aplicar auto formatação

--- resources/views/exercicio/index.blade.php
+++ resources/views/exercicio/index.blade.php
@@ -1,8 +1,8 @@
 <x-template-layout>
     @if ($message = Session::get('success'))
-    <div class="alert alert-dark" role="alert">
-        {{ $message }}
-    </div>
+        <div class="alert alert-dark" role="alert">
+            {{ $message }}
+        </div>
     @endif
     <div class="row mb-4">
         <div class="col-6">
@@ -17,47 +17,48 @@
         </div>
     </div>
     @if (empty($exercicios))
-    <p>Nenhum Exercicio cadastrado</p>
+        <p>Nenhum Exercicio cadastrado</p>
     @else
-    <div class="row mb-4">
-        <div class="col-12">
-            <table id="myTable" class="table align-middle">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Descricao</th>
-                        <th scope="col">Equipamentos</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($exercicios as $exercicio)
-                    <tr>
-                        <td scope="row">{{ $loop->index + 1 }}</td>
-                        <td>{{ $exercicio->nome }}</td>
-                        <td>{{ $exercicio->descricao }}</td>
-                        @if ($exercicio->equipamento)
-                        <td>{{ $exercicio->equipamento }}</td>
-                        @else
-                        <td>Sem necessidade de equipamentos.</td>
+        <div class="row mb-4">
+            <div class="col-12">
+                <table id="myTable" class="table align-middle">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Descricao</th>
+                            <th scope="col">Equipamentos</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($exercicios as $exercicio)
+                            <tr>
+                                <td scope="row">{{ $loop->index + 1 }}</td>
+                                <td>{{ $exercicio->nome }}</td>
+                                <td>{{ $exercicio->descricao }}</td>
+                                @if ($exercicio->equipamento)
+                                    <td>{{ $exercicio->equipamento }}</td>
+                                @else
+                                    <td>Sem necessidade de equipamentos.</td>
+                                @endif
+                                <td>
 
-                        @endif
-                        <td>
-
-                            <form action="{{ route('exercicio.destroy', $exercicio->id) }}" method="POST">
-                                <a href="{{ route('exercicio.edit', $exercicio->id) }}" class="me-2 btn btn-outline-dark"><i class="bi bi-pencil-square"></i></a>
-                                <a href="{{ route('exercicio.show', $exercicio->id) }}" class="me-2 btn btn-outline-info"><i class="bi bi-eye"></i></a>
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                    <form action="{{ route('exercicio.destroy', $exercicio->id) }}" method="POST">
+                                        <a href="{{ route('exercicio.edit', $exercicio->id) }}"
+                                            class="me-2 btn btn-outline-dark"><i class="bi bi-pencil-square"></i></a>
+                                        <a href="{{ route('exercicio.show', $exercicio->id) }}"
+                                            class="me-2 btn btn-outline-info"><i class="bi bi-eye"></i></a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     @endif
 </x-template-layout>
